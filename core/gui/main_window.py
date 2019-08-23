f3fb18bd8f853a2c2aba87deded2978838e3a70f
Gaudenz Halter <gaudenz.halter@live.com>
2019-08-23 08:35:22 +0200

    Removed Colorimetry auto start

--- core/gui/main_window.py
+++ core/gui/main_window.py
@@ -2161,20 +2161,20 @@ class MainWindow(QtWidgets.QMainWindow):
         run_colormetry = False
         if not ready:
             run_colormetry = False
-            if self.settings.AUTO_START_COLORMETRY:
-                run_colormetry = True
-            else:
-                self.open_dialogs.append("Dummy")
-                self.check_overlay_visibility()
-                answer = QMessageBox.question(self, "Colormetry",
-                                              "Do you want to start the Colormetry Analysis now?"
-                                              "\n\n"
-                                              "Hint: The Colormetry will be needed for several Tools in VIAN,\n"
-                                              "but will need quite some resources of your computer.")
-                if answer == QMessageBox.Yes:
-                    run_colormetry = True
-                self.open_dialogs.remove("Dummy")
-                self.check_overlay_visibility()
+            # if self.settings.AUTO_START_COLORMETRY:
+            #     run_colormetry = True
+            # else:
+            #     self.open_dialogs.append("Dummy")
+            #     self.check_overlay_visibility()
+            #     answer = QMessageBox.question(self, "Colormetry",
+            #                                   "Do you want to start the Colormetry Analysis now?"
+            #                                   "\n\n"
+            #                                   "Hint: The Colormetry will be needed for several Tools in VIAN,\n"
+            #                                   "but will need quite some resources of your computer.")
+            #     if answer == QMessageBox.Yes:
+            #         run_colormetry = True
+            #     self.open_dialogs.remove("Dummy")
+            #     self.check_overlay_visibility()
 
             if run_colormetry:
                 self.toggle_colormetry()
