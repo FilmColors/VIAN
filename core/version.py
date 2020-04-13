75d55c50a8652532a536b857fde415869d3f2c58
Gaudenz Halter <gaudenz.halter@live.com>
2020-04-13 14:57:20 +0200

    Updated Version

--- core/version.py
+++ core/version.py
@@ -1,6 +1,10 @@
 __author__ = "Gaudenz Halter"
-__copyright__ = "Copyright 2019, Gaudenz Halter"
-__credits__ = ["ERC Advanced Grant FilmColors ", "Gaudenz Halter", "FIWI, University of Zurich", "VMML, University of Zurich"]
+__copyright__ = "Copyright 2019, Barbara Flueckiger"
+__credits__ = [
+    "ERC Advanced Grant FilmColors ",
+    "Department of Film Studies, University of Zurich",
+    "Visualization and Multimedia Lab, University of Zurich"
+]
 __license__ = "GPL"
 __version__ = "0.8.16"
 __maintainer__ = "Gaudenz Halter"
