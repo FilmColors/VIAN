db9fcdfd2124e45325c6cf8eb5f393a06b29c281
Gaudenz Halter <gaudenz.halter@live.com>
2021-09-07 13:38:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-import glob
+import glob, os
 
 requirements = [
     'tensorflow',
@@ -22,42 +22,11 @@ requirements = [
     'pandas'
 ]
 
-print(glob.glob('vian/data/**', recursive=True))
 setup(
     name='VIAN',
     version='0.9.5',
-    packages=['vian.core',
-              'vian.core.gui',
-              'vian.core.gui.misc',
-              'vian.core.gui.dialogs',
-              'vian.core.gui.timeline',
-              'vian.core.data',
-              'vian.core.data.io',
-              'vian.core.analysis',
-              'vian.core.analysis.audio',
-              'vian.core.analysis.color',
-              'vian.core.analysis.colorimetry',
-              'vian.core.analysis.movie_mosaic',
-              'vian.core.analysis.deep_learning',
-              'vian.core.analysis.pipeline_scripts',
-              'vian.core.analysis.eyetracking',
-              'vian.core.analysis.motion',
-              'vian.core.container',
-              'vian.core.concurrent',
-              'vian.core.node_editor',
-              'vian.core.visualization',
-              'vian.extensions',
-              'vian.extensions.plugins',
-              'vian.extensions.plugins.fiwi_tools',
-              'vian.extensions.plugins.imdb_finder',
-              'vian.extensions.plugins.refactoring',
-              'vian.extensions.scripts',
-              'vian.extensions.analysis',
-              'vian.extensions.pipelines',
-              'vian.flask_server'
-              ],
+    packages=find_packages(),
     url='www.vian.app',
-    data_files=[('data', glob.glob('vian/data/**.*', recursive=True))],
     license='GPL',
     author='Gaudenz',
     install_requires=requirements,
